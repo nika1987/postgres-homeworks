c54ce7d9e718555e19ceebb2556dddfcb77c9ba2
Veronika <veronika.apakova@mail.ru>
2023-06-20 18:18:25 +0300

    create project1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,24 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+  employee_id serial PRIMARY KEY,
+  first_name varchar(100) NOT NULL,
+  last_name varchar(100),
+  title text,
+  birth_date date,
+  notes text
+);
+CREATE TABLE customers
+(
+	customer_id varchar(30) PRIMARY KEY,
+	company_name char (50),
+	contact_name char (50)
+);
+
+CREATE TABLE orders
+(order_id int PRIMARY KEY,
+customer_id varchar(20) REFERENCES customers(customer_id),
+ employee_id int REFERENCES employees(employee_id),
+ order_date date,
+ ship_city varchar(20)
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,31 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+
+import psycopg2
+
+with psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="goldbaby1987@") as conn:
+    with conn.cursor() as cur:
+        with open('./north_data/customers_data.csv') as file:
+            reader = csv.DictReader(file, delimiter=",").reader
+            next(reader)
+            for x in reader:
+                cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", x)
+            conn.commit()
+        with open('./north_data/employees_data.csv') as file:
+            reader = csv.DictReader(file, delimiter=",").reader
+            next(reader)
+            for x in reader:
+                print(x)
+                cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", x)
+            conn.commit()
+
+        with open('./north_data/orders_data.csv') as file:
+            reader = csv.DictReader(file, delimiter=",").reader
+            next(reader)
+            for x in reader:
+                cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", x)
+            conn.commit()
